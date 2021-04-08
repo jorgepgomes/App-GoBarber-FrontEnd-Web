601aea23121940202c734ad5c0f7f7d2df45170c
Jorge Pires <jorgewpgomes@gmail.com>
2021-04-08 01:07:47 -0300

    first commit

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,78 @@
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
+import { useField } from '@unform/core';
+
+import { Container, Error } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const handleIsFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={handleIsFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size="20" />
+        </Error>
+      )}
+    </Container>
+  );
+};
+
+export default Input;
+
+/**
+ * useCallback() => usada em toda funcao dentro de um componente para que ela nao seja renderizada toda vez,
+ *                  renderizando apenas quando um estado declarado é alterado
+ *
+ *
+ * sem lib @unform/core
+   const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
+    <Container>
+      {Icon && <Icon size={20} />}
+      <input {...rest} />
+    </Container>
+  );
+
+ */
